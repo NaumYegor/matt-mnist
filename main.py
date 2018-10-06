fd6f9b8ddb70474c318a4f5659cad023420cf153
naum <yehornaumenko@gmail.com>
2018-10-06 19:21:55 +0300

    22%... and main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-import  mnist
+import mnist
+import test
+import train
+
+K = 997
 mndata = mnist.MNIST('./images')
 train_images, train_labels = mndata.load_training()
 test_images, test_labels = mndata.load_testing()
+results = []
+
+results = train.train(train_images, train_labels, results)
+results = sorted(results, key=lambda image: image[0])  # Sorts by value
+
+print(test.test(test_images, test_labels, results, K))
