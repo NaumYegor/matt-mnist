aeec514d1721620a4987f84c0651a66cfa696433
naum <yehornaumenko@gmail.com>
2020-06-12 13:41:49 +0300

    For gregariy

--- main.py
+++ main.py
@@ -7,13 +7,17 @@ train_images, train_labels = mndata.load_training()
 test_images, test_labels = mndata.load_testing()
 result = sklearn.neighbors.KNeighborsClassifier(n_neighbors=3)
 
+print("Test point 1")
+
 fit_time = time.time()
+
+print("Test point 2")
 result.fit(train_images, train_labels)
 print("Fit time: " + str(time.time()-fit_time))
 
 predict_time = time.time()
 accuracy = 0
-test_set_length = len(test_images)
+test_set_length = 100
 
 for i in range(test_set_length):
     if result.predict([test_images[i]])[0] == test_labels[i]:
